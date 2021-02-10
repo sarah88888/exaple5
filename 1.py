d9c1e789ff818ab7a95d123843ca2073a0a139da
sara88888 <nawafsara88@gmail.com>
2021-02-10 16:50:13 +0300

    delete space

---
+++ 1.py
@@ -0,0 +1,26 @@
+start = int(input('enter Start number:'))
+end = int(input('enter end number:'))
+
+
+
+
+for less in range(start,end):
+    if less<20:
+        for less in range(20, 50):
+            print(less)
+
+    else:
+        for less in range(20, 50, 2):
+                print(less )
+    
+
+
+# write a simple code to take 2 numbers from the user and print the numbers between them
+start_user = int(input('enter Start::'))
+end_user = int(input('enter end::'))
+for between in range (start_user,end_user) :
+    print(between)
+
+
+
+
