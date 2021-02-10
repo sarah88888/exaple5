e48153ce2045ff0f2d208a3ac7a5d729c26e3fe2
saudidevorg <54324119+sarah88888@users.noreply.github.com>
2021-02-10 17:37:15 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,26 +1,30 @@
-start = int(input('enter Start number:'))
-end = int(input('enter end number:'))
-
-
-
-less = 0
-if less < 20:
-    for less in range(start, end,50):
-
-        for less in range(20, 50, 3):
-            print(less)
-
-    else:
-        for less in range(20, 50, 2):
-            print(less)
-    
-
-
 # write a simple code to take 2 numbers from the user and print the numbers between them
 start_user = int(input('enter Start::'))
 end_user = int(input('enter end::'))
 for between in range (start_user,end_user) :
     print(between)
+
+
+
+
+
+
+
+
+start = int(input('enter Start number:'))
+end = int(input('enter end number:'))
+
+
+
+
+for less in range(start,end,50):
+    if less<20:
+        for less in range(20, 50,3):
+            print(less)
+
+    else:
+        for less in range(20, 50, 2):
+                print(less )
 
 
 
